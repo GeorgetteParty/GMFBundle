c72a53e40079809717c1319dd25190d8bed8952b
Goutte <antoine.goutenoir@gmail.com>
2013-01-24 04:12:24 +0100

    Fixed Tests after update

--- Brick/ViewBrick.php
+++ Brick/ViewBrick.php
@@ -6,5 +6,13 @@ namespace Gmf\GmfBundle\Brick;
 
 class ViewBrick extends GameBrick
 {
-    
+
+    /**
+     * Use this to load data
+     * @return mixed
+     */
+    public function load()
+    {
+        // TODO: Implement load() method.
+    }
 }
