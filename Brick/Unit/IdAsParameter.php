5d6b581f217a02078dc4ec0ee1ec84a1b35ac322
Goutte <git is g0d>
2013-07-05 05:52:54 +0200

    Moved ODM annotations to traits

--- Brick/Unit/IdAsParameter.php
+++ Brick/Unit/IdAsParameter.php
@@ -2,20 +2,25 @@
 
 namespace GeorgetteParty\GMFBundle\Brick\Unit;
 
-use Exception;
 use GeorgetteParty\GMFBundle\Space\Coordinates;
 
 /**
- * Bare-bones way of having an id as parameter
+ * Bare-bones way of having an id as parameter,
+ * and providing the ODM with necessary annotations
  */
 trait IdAsParameter // implements Identifiable
 {
-    protected $id = null;
+
+    /**
+     * Auto-generated unique string ID, eg: '51d625e55688a9df2ab79f48'
+     * @ODM\Id
+     */
+    protected $id;
 
     public function getId()
     {
-        if (null == $this->id) {
-            throw new Exception("This object has no ID yet,");
+        if (null === $this->id) {
+            throw new \BadMethodCallException("This object has no ID yet,");
         }
 
         return $this->id;
