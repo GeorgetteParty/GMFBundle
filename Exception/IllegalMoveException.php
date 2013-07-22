0557d557e38ff59e9c8cfa939f4f45e8b13c4ae4
Goutte <antoine.goutenoir@gmail.com>
2013-07-23 01:43:36 +0200

    Added IllegalMoveResponse

--- Exception/IllegalMoveException.php
+++ Exception/IllegalMoveException.php
@@ -6,7 +6,7 @@ namespace GeorgetteParty\GMFBundle\Exception;
  * Can pass the array of coordinates (x,y,z) as second parameter,
  * it will be automatically dumped at the end of the message.
  *
- * todo: It should accept a Move, not coordinates.
+ * todo: It should accept a Move, not coordinates. (not sure about that)
  *
  * This is also a base class for you Exceptions.
  *
@@ -15,12 +15,23 @@ namespace GeorgetteParty\GMFBundle\Exception;
  */
 class IllegalMoveException extends \Exception
 {
+    protected $coordinates;
+
     public function __construct($message = "", $moveCoordinates = null, $code = 0, \Exception $previous = null)
     {
         if (is_array($moveCoordinates)) {
+            $this->coordinates = $moveCoordinates;
             $message .= ' (' . join(',', $moveCoordinates) . ')';
         }
 
         parent::__construct($message, $code, $previous);
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getCoordinates()
+    {
+        return $this->coordinates;
     }
 }
