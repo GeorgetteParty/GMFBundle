8019050882a22fcd5cd1fa18928be3d2dd0d9d3d
Goutte <antoine.goutenoir@gmail.com>
2014-05-26 04:58:10 +0000

    Cleanup in global_functions

--- global_functions.php
+++ global_functions.php
@@ -5,14 +5,10 @@
  * Some functions deserve a place as globals, when they're consistent with the inbuilt php functions.
  * Prefer Static methods when it makes sense.
  *
- * todo : find a way to add this dep. via composer
- *
- * Functions are sorted alphabetically.
+ * Functions should be sorted alphabetically.
  *
  * Maths deserve a place as global.
  * Array manipulation too.
- *
- *
  */
 
 
@@ -33,11 +29,12 @@ function array_push_once (&$array, $value)
 
 /**
  * Rotates passed array $offset times to the left, like a cycle would.
- * Only use this with numeric arrays.
+ * You can only use this with numerically-indexed arrays.
+ * Note that it mutates the provided array.
  *
  * Eg:
  * $a = array(7,8,9);
- * array_rotate_left($a,1); // $a == array(8,9,7)
+ * array_rotate_left($a,1); // array(8,9,7)
  *
  * @param array $array The array to rotate
  * @param int $offset When negative, rotates to the right
@@ -61,6 +58,21 @@ function array_rotate_left (&$array, $offset=0) {
 }
 
 
+/**
+ * Obvious
+ * We all wrote that line at some point.
+ * (Ongoing Poll: 1/1 = 100%)
+ *
+ * @param $value
+ * @param $min
+ * @param $max
+ * @return mixed
+ */
+function boundarize ($value, $min, $max)
+{
+    return min($max, max($min, $value));
+}
+
 
 /**
  * Is $n an even number ?
@@ -76,6 +88,7 @@ function even($n)
     return $n % 2 === 0;
 }
 
+
 /**
  * Is $n an odd number ?
  * The odd Zero is even !
@@ -87,7 +100,6 @@ function odd($n)
 {
     return !even($n);
 }
-
 
 
 /**
@@ -124,21 +136,4 @@ function sign ($number)
     }
     throw new Exception("Unsupported number $number");
 }
-
-
-/**
- * Obvious
- * We all wrote that line at some point.
- * (Ongoing Poll: 1/1 = 100%)
- *
- * @param $value
- * @param $min
- * @param $max
- * @return mixed
- */
-function boundarize ($value, $min, $max)
-{
-    return min($max, max($min, $value));
-}
-
 
